90d8514e0e41be6f8db9e1dcbffeada936275865
CodeVineDev <codevine2023@gmail.com>
2025-07-15 22:55:11 -0700

    form validation

---
+++ project 6/app.js
@@ -0,0 +1,9 @@
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const pswd = document.getElementById("password");
+const confirmPswd = document.getElementById("name");
+const submit = document.getElementById("submit");
+
+
+submit.addEventListener("click", ()=>{
+})
