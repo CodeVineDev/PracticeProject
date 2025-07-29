cd719186f42ad6541aa1bbe44e72eb2df42eefd1
CodeVineDev <codevine2023@gmail.com>
2025-07-28 22:24:48 -0700

    slug url

--- project8/app.js
+++ project8/app.js
@@ -1,18 +1,19 @@
-fetch(
-  "https://api.storyblok.com/v2/cdn/stories/my-home?version=draft&token=QByTQEQaL8cMuMXr6NQ8eQtt"
-)
+const params = new URLSearchParams(window.location.search);
+const slug = params.get("slug") || "my-home"; // fallback to 'my-home' if not provided
+
+fetch(`https://api.storyblok.com/v2/cdn/stories/${slug}?version=draft&token=QByTQEQaL8cMuMXr6NQ8eQtt`)
   .then((res) => res.json())
   .then((data) => {
     const bodyBlocks = data.story.content.body;
 
-    // Find the block with the component name "home-section"
-    const homeSection = bodyBlocks.find(
+    // Find the block with the component name "hero-section"
+    const heroSection = bodyBlocks.find(
       (block) => block.component === "hero-section"
     );
 
-    if (homeSection) {
-      const title = homeSection.title;
-      const desc = homeSection.description;
+    if (heroSection) {
+      const title = heroSection.title;
+      const desc = heroSection.description;
 
       document.getElementById("title").textContent = title;
       document.getElementById("desc").textContent = desc;
